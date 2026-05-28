fb74816cee322d35a00b12507f93d839fe5139d9
Yan Portero <yanweiporteroguzman@gmail.com>
2026-05-27 21:59:46 -0500

    Complete config vite and deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import './App.css'
+import { Grid } from '@mui/material';
 
 function App() {
-    return (
-        <div>
-            <h1>Bienvenido al Dashboard</h1>
-        </div>
-    );
+   return (
+      <Grid container spacing={5} sx={{ justifyContent: "center", alignItems: "center" }}>
+
+         {/* Encabezado */}
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+
+         {/* Alertas */}
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
+
+         {/* Selector */}
+         <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
+
+         {/* Indicadores */}
+         <Grid size={{ xs: 12, md: 9 }}>Elemento: Indicadores</Grid>
+
+         {/* Gráfico */}
+         <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>
+            Elemento: Gráfico
+         </Grid>
+
+         {/* Tabla */}
+         <Grid size={{ xs: 12, md: 6 }} sx={{ display: { xs: "none", md: "block" } }}>
+            Elemento: Tabla
+         </Grid>
+
+         {/* Información adicional */}
+         <Grid size={{ xs: 12, md: 12 }}>Elemento: Información adicional</Grid>
+
+      </Grid>
+   );
 }
 
 export default App;
